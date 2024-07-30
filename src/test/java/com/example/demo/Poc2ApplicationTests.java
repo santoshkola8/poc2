d17c19d3b11789233727f3583e18bea4b4658101
Santosh Kola <santoshkola8@gmail.com>
2024-07-30 12:54:47 -0400

    initial push

---
+++ src/test/java/com/example/demo/Poc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
